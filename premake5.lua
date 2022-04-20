93b63c1b92085e8e5a852261dbf23bae0f02a153
Juha Lehtinen <jtlehtinen@fastmail.com>
2022-04-20 13:38:51 +0300

    project setup

---
+++ premake5.lua
@@ -0,0 +1,30 @@
+workspace "paceit"
+  location "project/"
+  configurations {"debug", "release"}
+  language "C++"
+  cppdialect "C++20"
+  flags { "MultiProcessorCompile", "FatalWarnings" }
+
+filter { "system:windows" }
+  platforms { "win64" }
+
+filter { "platforms:Win64" }
+defines { "WIN32", "_CRT_SECURE_NO_WARNINGS", "WIN32_LEAN_AND_MEAN", "NOMINMAX" }
+architecture "x64"
+
+filter "configurations:debug"
+  defines { "METRONOME_DEBUG" }
+  symbols "On"
+
+filter "configurations:release"
+  optimize "On"
+
+project "paceit"
+  location "project/"
+  kind "ConsoleApp"
+  targetdir "project/bin/%{cfg.platform}/%{cfg.buildcfg}"
+  files {"src/**.h", "src/**.cpp"}
+  includedirs { "src" }
+  editandcontinue "Off"
+  staticruntime "On"
+  flags { "NoIncrementalLink", "NoPCH" }
---
+++ src/main.cpp
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+  printf("paceit\n");
+  return 0;
+}
